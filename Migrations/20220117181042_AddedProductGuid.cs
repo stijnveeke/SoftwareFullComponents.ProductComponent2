971eafe64113148495b8882eef0b0ba5f9f1c230
Stitch <st.van.eekelen@gmail.com>
2022-01-17 20:13:48 +0100

    Changed produc component now fully implementing getting product by guid

--- Migrations/20220117181042_AddedProductGuid.cs
+++ Migrations/20220117181042_AddedProductGuid.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace SoftwareFullComponents.Product2Component.Migrations
 {
@@ -6,11 +7,12 @@ namespace SoftwareFullComponents.Product2Component.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
+            migrationBuilder.AddColumn<Guid>(
                 name: "Guid",
                 table: "Product",
-                type: "nvarchar(max)",
-                nullable: true);
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
